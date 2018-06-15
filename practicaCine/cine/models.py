56311cb5713a17eff7d1ff2b7340a86f335cf2ea
Victor Dominguez <riespri93@gmail.com>
2018-06-15 16:27:21 +0200

    Models

--- practicaCine/cine/models.py
+++ practicaCine/cine/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Comentario(models.Model):
+    titulo = models.CharField(max_length=200)
+    opinion = models.CharField(max_length=200)
+    
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=200)
+    director = models.CharField(max_length=200)
+    genero = models.CharField(max_length=200)
+    sinopsis = models.CharField(max_length=200)
+    foto = models.ImageField()
+    comentario = models.ForeignKey(Comentario, on_delete= models.CASCADE)
